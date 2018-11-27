91effdaefb3967da02d46dccd44de9f067e27c2e
Yassine <yasine.nifa@mail.com>
2018-11-27 18:53:12 +0100

    Create all main activity 's fragments

--- app/src/main/java/yassine/com/devchatapp/MainActivity.java
+++ app/src/main/java/yassine/com/devchatapp/MainActivity.java
@@ -1,31 +1,113 @@
 package yassine.com.devchatapp;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsPagerAdapter mTabsPagerAdapter;
+    private FirebaseUser currentUser;
+    private DatabaseReference UsersReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String online_user_id = mAuth.getCurrentUser().getUid();
+            UsersReference = FirebaseDatabase.getInstance().getReference().child("Users").child(online_user_id);
+        }
+
+        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        mTabsPagerAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(mTabsPagerAdapter);
+        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        //mToolbar.setTitle("myChat");
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("DevChat");
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser == null){
-            Intent startPageIntent = new Intent(MainActivity.this, StartPageActivity.class);
-            startPageIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(startPageIntent);
-            finish();
+        currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            LogOutUser();
+
+        } else if (currentUser != null) {
+            UsersReference.child("online").setValue("true");
         }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (currentUser != null) {
+            UsersReference.child("online").setValue(ServerValue.TIMESTAMP);
+        }
+    }
+
+    private void LogOutUser() {
+        Intent startPageIntent = new Intent(MainActivity.this, StartPageActivity.class);
+        startPageIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(startPageIntent);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.mainlogout_button) {
+
+            if (currentUser != null) {
+                UsersReference.child("online").setValue(ServerValue.TIMESTAMP);
+            }
+            mAuth.signOut();
+            LogOutUser();
+
+        }
+        if (item.getItemId() == R.id.main_account_setting_button_button) {
+            Intent settingdIntent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(settingdIntent);
+
+        }
+
+
+        if (item.getItemId() == R.id.main_all_users_button) {
+            Intent AllUsersIntent = new Intent(MainActivity.this, AllUsersActivity.class);
+            startActivity(AllUsersIntent);
+        }
+
+
+        return true;
+
     }
 }
---
+++ app/src/main/java/yassine/com/devchatapp/TabsPagerAdapter.java
@@ -0,0 +1,49 @@
+package yassine.com.devchatapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Mohammed on 27/11/2018.
+ */
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                RequestsFragment requestsFragment = new RequestsFragment();
+                return requestsFragment;
+            case 1 :
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+            case 2:
+                FriendsFragment friendsFragment = new FriendsFragment();
+                return friendsFragment;
+            default :
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+    public CharSequence getPageTitle(int position){
+        switch (position){
+            case 0:
+                return "Requests";
+            case 1:
+                return "Chats";
+            case 2:
+                return "Friends";
+            default:
+                return null;
+        }
+    }
+}
